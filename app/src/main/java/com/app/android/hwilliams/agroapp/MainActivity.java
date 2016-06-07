04b1fe351f49638bf4da496a730de56c70d0b172
Hernan <Hernan@Home>
2016-06-07 00:58:03 -0300

    nuevas cosas

--- app/src/main/java/com/app/android/hwilliams/agroapp/MainActivity.java
+++ app/src/main/java/com/app/android/hwilliams/agroapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.app.android.hwilliams.agroapp;
 
+import android.app.FragmentManager;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+//        googleMap.setMyLocationEnabled(true);
+//        centerMapOnUserLocation(googleMap);
+    }
+
+    private void centerMapOnUserLocation(GoogleMap googleMap){
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null) {
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(17).bearing(90).tilt(40).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
     }
 }
